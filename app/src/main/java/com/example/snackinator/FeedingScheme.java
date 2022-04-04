546b97f819eb57005ff842e4ac0cc9f113eb1bb4
soniadeca28 <sonia.deca@student.upt.ro>
2022-04-05 00:57:47 +0300

    Added water fountain switch, save button and functional back button

--- app/src/main/java/com/example/snackinator/FeedingScheme.java
+++ app/src/main/java/com/example/snackinator/FeedingScheme.java
@@ -4,16 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TimePicker;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
 
 public class FeedingScheme extends AppCompatActivity {
 
-    Button breakfastTimeButton, lunchTimeButton, dinnerTimeButton;
+    Button breakfastTimeButton, lunchTimeButton, dinnerTimeButton, backToHomeButton, waterChoiceButton;
     int hour,minute;
 
     @Override
@@ -29,6 +32,16 @@ public class FeedingScheme extends AppCompatActivity {
 
         dinnerTimeButton = findViewById(R.id.dinnerTime);
         dinnerTimeButton.setOnClickListener(v->timePicker(v,dinnerTimeButton));
+        
+        backToHomeButton = findViewById(R.id.backButton);
+        backToHomeButton.setOnClickListener(v->backToHomePage());
+
+        waterChoiceButton = findViewById(R.id.waterChoice);
+    }
+
+    private void backToHomePage() {
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 
     public void timePicker(View view, Button btn)
